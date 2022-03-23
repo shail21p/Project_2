d02c695705486a0f4af8bd81638c493690425c52
shail patel <sp233@njit.edu>
2022-03-23 13:34:45 -0400

    page4

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,7 +4,6 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
     assert b'<a class="nav-link" href="/page1">Page 1</a>' in response.data
     assert b'<a class="nav-link" href="/page2">Page 2</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Page 3</a>' in response.data
@@ -16,11 +15,7 @@ def test_request_index(client):
     assert response.status_code == 200
     assert b"Index Page" in response.data
 
-def test_request_about(client):
-    """This makes the index page"""
-    response = client.get("/about")
-    assert response.status_code == 200
-    assert b"About Page" in response.data
+
 
 def test_request_page1(client):
     """This makes the index page"""
